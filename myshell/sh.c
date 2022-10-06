460edb6cc3c4ef30d88dc56223c37d19b6462fbd
ClayWW <claywolfang@gmail.com>
2022-10-05 22:30:35 -0400

    Prelim versions of which and where

--- myshell/sh.c
+++ myshell/sh.c
@@ -127,17 +127,64 @@ int sh( int argc, char **argv, char **envp )
   return 0;
 } /* sh() */
 
-//char *which(char *command, struct pathelement *pathlist )
-//{
+char *which(char *command, struct pathelement *pathlist )
+{
+  struct pathelement *currentpath = pathlist;                //the current pathlist
+  DIR *givenDir;                                             //the directory we were given
+  struct dirent *rawDir;                                     //the variable that holds the pointer resulting from readdir
+  char fullpath[BUFFER_SIZE];                                //a large char variable to hold the full path name
+
+  while(currentpath){
+    char *currentpathelement = currentpath->element;
+    givenDir = opendir(currentpathelement);
+    if(givenDir){
+      while((rawDir = readdir(givenDir)) != NULL){
+        if(strcmp(rawDir->d_name, command) == 0){
+          strcpy(fullpath, currentpathelement);
+          strcat(fullpath, "/");
+          strcat(fullpath, rawDir->d_name);
+          closedir(givenDir);
+          return fullpath;                                    //**
+        }
+      }
+    }
+    closedir(givenDir);
+    currentpath = currentpath->next;
+  }
+  return(NULL);
    /* loop through pathlist until finding command and return it.  Return
    NULL when not found. */
 
-//} /* which() */
+} /* which() */
 
-//char *where(char *command, struct pathelement *pathlist )
-//{
+char *where(char *command, struct pathelement *pathlist )
+{
+  struct pathelement *currentpath = pathlist;                //the current pathlist
+  DIR *givenDir;                                             //the directory we were given
+  struct dirent *rawDir;                                     //the variable that holds the pointer resulting from readdir
+  char alllocations[BUFFER_SIZE];                            //a large char variable to hold all of the locations of the command
+
+  while(currentpath){                                        //while the current pathlist still exists (isn't NULL)
+      char *currentpathelement = currentpath->element;       //the element associated with the node at the current path
+      givenDir = opendir(currentpathelement);                //open the directory that is associated in the element at the current point in the pathlist
+      if(givenDir){                                          //if the given directory at the current path element is not null
+          while((rawDir = readdir(givenDir)) != NULL){       //while loop that executes so long as the pointer returned in readdir is not returned as NULL
+              if(strcmp(rawDir->d_name, command) == 0){      //if the entry in the directory matches the command we're looking for,
+                strcat(alllocations, currentpathelement);    // then add it to the alllocations char variable (with formatting)
+                strcat(alllocations, "/");
+                strcat(alllocations, rawDir->d_name);
+                strcat(alllocations, "\n");
+              }
+          }
+      }
+      closedir(givenDir);                                    //close the given directory after use
+      currentpath = currentpath->next;                       //move to the next node in the pathlist
+  }
+                                                            //**
+  return alllocations;                                      //return all of the locations where command was found
+
   /* similarly loop through finding all locations of command */
-//} /* where() */
+} /* where() */
 
 void list ( char *dir )
 {
@@ -154,6 +201,7 @@ void list ( char *dir )
     }
   }
   closedir(givenDir);                               //need to close the directory we opened
+
   /* see man page for opendir() and readdir() and print out filenames for
   the directory passed */
 } /* list() */
